fd6bc010dabd3a60cf0715096ac00301aae65118
SUDARSHANA V <140592185+VSudarshana@users.noreply.github.com>
2024-05-28 19:02:41 +0530

    Time: 1 ms (96.65%), Space: 42.5 MB (77.15%) - LeetHub

--- 3046-split-the-array/3046-split-the-array.java
+++ 3046-split-the-array/3046-split-the-array.java
@@ -1,15 +1,40 @@
+// class Solution {
+//     public boolean isPossibleToSplit(int[] nums) {
+//         if (nums.length % 2 == 0) {
+//             return true;
+//         }
+//         int nums1[] = new int[nums.length/2];
+//         int nums2[] = new int[nums.length/2];
+//         Arrays.sort(nums);
+//         for (int i = 0; i < nums.length; i++) {
+//             for (int j = i + 1; j < nums.length; j++) {
+//                 if (nums[i] != nums[j]) {
+//                     nums1[i] = nums[i];
+//                 } else {
+//                     nums2[i] = nums[i];
+//                 }
+//                 if (nums1[i] != nums1[j]) {
+//                     return true;
+//                 }
+//             }
+
+//         }
+//         return false;
+//     }
+// }
+
 class Solution {
     public boolean isPossibleToSplit(int[] nums) {
-        int n=nums.length;int cnt=0;
-        for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
-                if(nums[i]==nums[j])
-                cnt++;
-                if(cnt>1)
-                return false;
-               
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            int cnt = 0;
+            for (int j = i + 1; j < n; j++) {
+                if (nums[i] == nums[j])
+                    cnt++;
+                if (cnt > 1)
+                    return false;
+
             }
-            cnt=0;
         }
         return true;
     }
